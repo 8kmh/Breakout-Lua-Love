17f59f41745f16174cc7ad440704d8c32183cb2b
Maida <maida.maxime@gmail.com>
2023-01-18 22:07:51 +0100

    Create ball, ball stick to the pad

--- main.lua
+++ main.lua
@@ -4,13 +4,27 @@ pad.y = 0
 pad.width = 80
 pad.height = 20
 
+local ball = {}
+ball.x = 0
+ball.y = 0
+ball.radius = 10
+ball.stick = false
+
+function start()
+  ball.stick = true
+end
+
 
 
 function love.load()
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
   
+  -- Set the pad to the bottom of the screen
   pad.y = height - (pad.height / 2)
+  
+  start()
+  
 end
 
 
@@ -18,11 +32,15 @@ function love.update(dt)
   -- Pad follow mouse
   pad.x = love.mouse.getX()
   
-  if pad.x + pad.width == width then
+  -- Stick the ball to the pad when ball.stick == true
+  if ball.stick == true then
+    ball.y = pad.y - pad.height / 2 - ball.radius
+    ball.x = pad.x
   end
 
 end
 
 function love.draw()
   love.graphics.rectangle("fill", pad.x - (pad.width / 2), pad.y - (pad.height / 2), pad.width, pad.height)
+  love.graphics.circle("fill", ball.x, ball.y, ball.radius)
 end
